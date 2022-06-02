af2e492e4e9456d1bef08a0d9d431394f4a21ea5
Jimmy Briggs <jimmy.briggs@jimbrig.com>
2022-06-01 20:53:31 -0400

    config: re-factor date utilities

--- R/utils_dates.R
+++ R/utils_dates.R
@@ -1,17 +1,23 @@
+#' Date Utility Functions
+#'
+#' @description
+#' Date utility helpers for deriving start/end dates.
+#'
+#' @name date_utils
+#'
+#' @param date Character string or Date representing the date to manipulate.
+#'
+#' @return Returns the Start or End Date as a Date.
+#'
+#' @importFrom lubridate is.Date ceiling_date floor_date
+#'
+#' @example examples/date_utils.R
+NULL
+
 #' End of Month
 #'
-#' @param date character or date representation of a date.
-#'
-#' @return date
 #' @export
-#'
-#' @importFrom lubridate is.Date ceiling_date
-#'
-#' @examples
-#' # character input
-#' end_of_month("2020-08-13")
-#' # date input
-#' end_of_month(as.Date("2020-08-13"))
+#' @rdname date_utils
 end_of_month <- function(date) {
   if (is.character(date)) date <- as.Date(date)
   stopifnot(lubridate::is.Date(date))
@@ -20,20 +26,18 @@ end_of_month <- function(date) {
 
 #' Beginning of Month
 #'
-#' @param date character or date representation of a date.
-#'
-#' @return date
 #' @export
-#'
-#' @importFrom lubridate is.Date floor_date
-#'
-#' @examples
-#' # character input
-#' beg_of_month("2020-08-13")
-#' # date input
-#' beg_of_month(as.Date("2020-08-13"))
+#' @rdname date-utils
 beg_of_month <- function(date) {
   if (is.character(date)) date <- as.Date(date)
   stopifnot(lubridate::is.Date(date))
   lubridate::floor_date(date, unit = "months")
+}
+
+#' Start of Month
+#'
+#' @export
+#' @rdname date-utils
+start_of_month <- function(date) {
+  beg_of_month(date)
 }
