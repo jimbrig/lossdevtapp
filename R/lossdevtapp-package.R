5896b3f5f119d1d490149b005dd599d262db1315
Jimmy Briggs <jimmy.briggs@jimbrig.com>
2022-06-01 21:13:06 -0400

    doc: add package documentation

---
+++ R/lossdevtapp-package.R
@@ -0,0 +1,23 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
+NULL
+
+utils::globalVariables(
+  c(
+    "AYE",
+    "age",
+    "cdfs",
+    "devt",
+    "earned_ratio",
+    "loss_dat",
+    "loss_data_all",
+    "origin value",
+    "value_lead",
+    "value_lead_total",
+    "value_total"
+  )
+)
+
